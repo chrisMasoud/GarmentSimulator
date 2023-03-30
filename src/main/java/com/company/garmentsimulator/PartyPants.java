71d236e746effb8e2fa5fc369d6537188d7335f0
chrisMasoud <chrismasoud22@gmail.com>
2023-03-30 11:00:33 -0400

    Added comments to PartyPants class

--- src/main/java/com/company/garmentsimulator/PartyPants.java
+++ src/main/java/com/company/garmentsimulator/PartyPants.java
@@ -1,6 +1,14 @@
 package com.company.garmentsimulator;
 
+/**
+ * The PartyPants class sews party pants
+ * @author Chris Masoud
+ */
 public class PartyPants implements Pants {
+    
+    /**
+     * The sew() method outputs that it is sewing party pants
+     */
     @Override
     public void sew() {
         System.out.println("Sewing party pants...");
