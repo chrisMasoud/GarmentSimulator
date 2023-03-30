fb9db741b642a9ab48a6363e5162513400d7d93b
chrisMasoud <chrismasoud22@gmail.com>
2023-03-30 11:06:51 -0400

    Added comments to GarmentFactory interface

--- src/main/java/com/company/garmentsimulator/GarmentFactory.java
+++ src/main/java/com/company/garmentsimulator/GarmentFactory.java
@@ -1,5 +1,9 @@
 package com.company.garmentsimulator;
 
+/**
+ * The GarmentFactory interface has methods for creating tops, pants, and shoes
+ * @author Chris Masoud
+ */
 public interface GarmentFactory {
     Top createTop();
     Pants createPants();
