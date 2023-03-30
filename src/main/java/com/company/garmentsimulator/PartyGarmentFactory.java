a129296498a1a2250b9a582d551408221ce195cd
chrisMasoud <chrismasoud22@gmail.com>
2023-03-30 10:54:28 -0400

    Added comments to PartyGarmentFactory class

--- src/main/java/com/company/garmentsimulator/PartyGarmentFactory.java
+++ src/main/java/com/company/garmentsimulator/PartyGarmentFactory.java
@@ -1,16 +1,33 @@
 package com.company.garmentsimulator;
 
+/**
+ * The PartyGarmentFactory class creates party tops, pants, and shoes
+ * @author Chris Masoud
+ */
 public class PartyGarmentFactory implements GarmentFactory {
+    
+    /**
+     * The createTop() method returns a new party top
+     * @return Top
+     */
     @Override
     public Top createTop() {
         return new PartyTop();
     }
 
+    /**
+     * The createPants() method returns new party pants
+     * @return Pants
+     */
     @Override
     public Pants createPants() {
         return new PartyPants();
     }
 
+    /**
+     * The createShoes() method returns new party shoes
+     * @return Shoes
+     */
     @Override
     public Shoes createShoes() {
         return new PartyShoes();
