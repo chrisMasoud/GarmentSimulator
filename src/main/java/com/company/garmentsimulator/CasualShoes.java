96586709128d4a896abac9544d90da268be4961e
chrisMasoud <chrismasoud22@gmail.com>
2023-03-30 10:37:16 -0400

    Created CasualShoes class

---
+++ src/main/java/com/company/garmentsimulator/CasualShoes.java
@@ -0,0 +1,8 @@
+package com.company.garmentsimulator;
+
+public class CasualShoes implements Shoes {
+    @Override
+    public void sew() {
+        System.out.println("Sewing casual shoes...");
+    }
+}
