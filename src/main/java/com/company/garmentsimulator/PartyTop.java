e7d8eee31760ed8c5a07fce8501d56d714da89b2
chrisMasoud <chrismasoud22@gmail.com>
2023-03-30 11:01:44 -0400

    Added comments to PartyTop class

--- src/main/java/com/company/garmentsimulator/PartyTop.java
+++ src/main/java/com/company/garmentsimulator/PartyTop.java
@@ -1,6 +1,14 @@
 package com.company.garmentsimulator;
 
+/**
+ * The PartyTop class sews a party top
+ * @author Chris Masoud
+ */
 public class PartyTop implements Top {
+    
+    /**
+     * The sew() method outputs that it is sewing a party top
+     */
     @Override
     public void sew() {
         System.out.println("Sewing a party top...");
