30754d1b863d94b23b0a2857e04377596d26b26b
chrisMasoud <chrismasoud22@gmail.com>
2023-03-30 11:04:07 -0400

    Added comments to ProfessionalTop class

--- src/main/java/com/company/garmentsimulator/ProfessionalTop.java
+++ src/main/java/com/company/garmentsimulator/ProfessionalTop.java
@@ -1,6 +1,14 @@
 package com.company.garmentsimulator;
 
+/**
+ * The ProfessionalTop class sews a professional top
+ * @author Chris Masoud
+ */
 public class ProfessionalTop implements Top {
+    
+    /**
+     * The sew() method outputs that it is sewing a professional top
+     */
     @Override
     public void sew() {
         System.out.println("Sewing a professional top...");
