5c875b1fa64b45b23f6928fb3c7415447ee1fb4f
chrisMasoud <chrismasoud22@gmail.com>
2023-03-30 11:02:53 -0400

    Added comments to ProfessionalPants class

--- src/main/java/com/company/garmentsimulator/ProfessionalPants.java
+++ src/main/java/com/company/garmentsimulator/ProfessionalPants.java
@@ -1,6 +1,14 @@
 package com.company.garmentsimulator;
 
+/**
+ * The ProfessionalPants class sews professional pants
+ * @author Chris Masoud
+ */
 public class ProfessionalPants implements Pants {
+    
+    /**
+     * The sew() method outputs that it is sewing professional pants
+     */
     @Override
     public void sew() {
         System.out.println("Sewing professional pants...");
