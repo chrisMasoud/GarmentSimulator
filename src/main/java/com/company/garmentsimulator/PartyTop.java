565898bddfa8c14aa590471cdc506c86540685b9
chrisMasoud <chrismasoud22@gmail.com>
2023-03-30 10:37:59 -0400

    Created PartyTop class

---
+++ src/main/java/com/company/garmentsimulator/PartyTop.java
@@ -0,0 +1,8 @@
+package com.company.garmentsimulator;
+
+public class PartyTop implements Top {
+    @Override
+    public void sew() {
+        System.out.println("Sewing a party top...");
+    }
+}
