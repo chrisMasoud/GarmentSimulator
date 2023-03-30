737a5736a5acfa7e6869031eb1446d113e77dc8f
chrisMasoud <chrismasoud22@gmail.com>
2023-03-30 10:59:28 -0400

    Added comments to CasualTop class

--- src/main/java/com/company/garmentsimulator/CasualTop.java
+++ src/main/java/com/company/garmentsimulator/CasualTop.java
@@ -1,6 +1,14 @@
 package com.company.garmentsimulator;
 
+/**
+ * The CasualTop class sews a casual top
+ * @author Chris Masoud
+ */
 public class CasualTop implements Top {
+    
+    /**
+     * The sew() method outputs that it is sewing a casual top
+     */
     @Override
     public void sew() {
         System.out.println("Sewing a casual top...");
