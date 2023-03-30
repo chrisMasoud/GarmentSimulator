3e5e7e220178aabe2f8e053997b0b34a09064e84
chrisMasoud <chrismasoud22@gmail.com>
2023-03-30 11:03:42 -0400

    Added comments to ProfessionalShoes class

--- src/main/java/com/company/garmentsimulator/ProfessionalShoes.java
+++ src/main/java/com/company/garmentsimulator/ProfessionalShoes.java
@@ -1,6 +1,14 @@
 package com.company.garmentsimulator;
 
+/**
+ * The ProfessionalShoes class sews professional shoes
+ * @author Chris Masoud
+ */
 public class ProfessionalShoes implements Shoes {
+    
+    /**
+     * The sew() method outputs that it is sewing professional shoes
+     */
     @Override
     public void sew() {
         System.out.println("Sewing professional shoes...");
