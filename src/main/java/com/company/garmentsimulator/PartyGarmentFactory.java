b392e2308848e4ebc8809392686e671801e1036f
chrisMasoud <chrismasoud22@gmail.com>
2023-03-30 10:32:29 -0400

    Created PartyGarmentFactory class

---
+++ src/main/java/com/company/garmentsimulator/PartyGarmentFactory.java
@@ -0,0 +1,18 @@
+package com.company.garmentsimulator;
+
+public class PartyGarmentFactory {
+    @Override
+    public Top createTop() {
+        return new PartyTop();
+    }
+
+    @Override
+    public Pants createPants() {
+        return new PartyPants();
+    }
+
+    @Override
+    public Shoes createShoes() {
+        return new PartyShoes();
+    }
+}
