7457c3418ac6cc24af4f719c2d8b47431849f638
chrisMasoud <chrismasoud22@gmail.com>
2023-03-30 10:35:05 -0400

    Created ProfessionalShoes class

---
+++ src/main/java/com/company/garmentsimulator/ProfessionalShoes.java
@@ -0,0 +1,8 @@
+package com.company.garmentsimulator;
+
+public class ProfessionalShoes implements Shoes {
+    @Override
+    public void sew() {
+        System.out.println("Sewing professional shoes...");
+    }
+}
