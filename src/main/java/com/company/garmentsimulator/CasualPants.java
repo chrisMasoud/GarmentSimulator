8800e4873e818198bb46b5027e5105a8ab0ef7e7
chrisMasoud <chrismasoud22@gmail.com>
2023-03-30 10:58:06 -0400

    Added comments to CasualPants class

--- src/main/java/com/company/garmentsimulator/CasualPants.java
+++ src/main/java/com/company/garmentsimulator/CasualPants.java
@@ -1,6 +1,14 @@
 package com.company.garmentsimulator;
 
+/**
+ * The CasualPants class sews casual pants
+ * @author Chris Masoud
+ */
 public class CasualPants implements Pants {
+    
+    /**
+     * The sew() method outputs that it is sewing casual pants
+     */
     @Override
     public void sew() {
         System.out.println("Sewing casual pants...");
