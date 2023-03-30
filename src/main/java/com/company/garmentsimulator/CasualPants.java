9b04e113c758770b52694ce1b088506850ee6b4f
chrisMasoud <chrismasoud22@gmail.com>
2023-03-30 10:36:39 -0400

    Created CasualPants class

---
+++ src/main/java/com/company/garmentsimulator/CasualPants.java
@@ -0,0 +1,8 @@
+package com.company.garmentsimulator;
+
+public class CasualPants implements Pants {
+    @Override
+    public void sew() {
+        System.out.println("Sewing casual pants...");
+    }
+}
