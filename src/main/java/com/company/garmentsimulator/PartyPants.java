39525f744f5abb5dbc108dedb51f6dbc1145615b
chrisMasoud <chrismasoud22@gmail.com>
2023-03-30 10:38:34 -0400

    Created PartyPants class

---
+++ src/main/java/com/company/garmentsimulator/PartyPants.java
@@ -0,0 +1,8 @@
+package com.company.garmentsimulator;
+
+public class PartyPants implements Pants {
+    @Override
+    public void sew() {
+        System.out.println("Sewing party pants...");
+    }
+}
