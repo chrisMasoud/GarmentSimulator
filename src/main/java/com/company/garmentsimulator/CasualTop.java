68825686aa49f3989d57fc742ed7551c7079a258
chrisMasoud <chrismasoud22@gmail.com>
2023-03-30 10:36:05 -0400

    Created CasualTop class

---
+++ src/main/java/com/company/garmentsimulator/CasualTop.java
@@ -0,0 +1,9 @@
+package com.company.garmentsimulator;
+
+public class CasualTop implements Top {
+    @Override
+    public void sew() {
+        System.out.println("Sewing a casual top...");
+    }
+}
+
