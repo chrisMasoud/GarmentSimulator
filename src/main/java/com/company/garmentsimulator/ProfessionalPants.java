c79878d9ace7f7023ba35d71a4a690cb732ce00b
chrisMasoud <chrismasoud22@gmail.com>
2023-03-30 10:34:24 -0400

    Created ProfessionalPants class

---
+++ src/main/java/com/company/garmentsimulator/ProfessionalPants.java
@@ -0,0 +1,8 @@
+package com.company.garmentsimulator;
+
+public class ProfessionalPants implements Pants {
+    @Override
+    public void sew() {
+        System.out.println("Sewing professional pants...");
+    }
+}
