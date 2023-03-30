526f805a9bb7745889f391f2b2e650aff8cfd5da
chrisMasoud <chrismasoud22@gmail.com>
2023-03-30 10:39:09 -0400

    Created PartyShoes class

---
+++ src/main/java/com/company/garmentsimulator/PartyShoes.java
@@ -0,0 +1,8 @@
+package com.company.garmentsimulator;
+
+public class PartyShoes implements Shoes {
+    @Override
+    public void sew() {
+        System.out.println("Sewing party shoes...");
+    }
+}
