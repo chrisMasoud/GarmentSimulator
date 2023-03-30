df182592b9a6939505926f8ef5e397eaedb9e066
chrisMasoud <chrismasoud22@gmail.com>
2023-03-30 10:55:58 -0400

    Added comments to ProfessionalGarmentFactory class

--- src/main/java/com/company/garmentsimulator/ProfessionalGarmentFactory.java
+++ src/main/java/com/company/garmentsimulator/ProfessionalGarmentFactory.java
@@ -1,17 +1,33 @@
-
 package com.company.garmentsimulator;
 
+/**
+ * The ProfessionalGarmentFactory class creates professional tops, pants, and shoes
+ * @author Chris Masoud
+ */
 public class ProfessionalGarmentFactory implements GarmentFactory {
+    
+    /**
+     * The createTop() method returns a new professional top
+     * @return Top
+     */
     @Override
     public Top createTop() {
         return new ProfessionalTop();
     }
 
+    /**
+     * The createPants() method returns new professional pants
+     * @return Pants
+     */
     @Override
     public Pants createPants() {
         return new ProfessionalPants();
     }
 
+    /**
+     * The createShoes() method returns new professional shoes
+     * @return Shoes
+     */
     @Override
     public Shoes createShoes() {
         return new ProfessionalShoes();
