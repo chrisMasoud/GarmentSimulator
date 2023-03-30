715a87e28fa082733e000673a626378b54d18c7f
chrisMasoud <chrismasoud22@gmail.com>
2023-03-30 10:52:38 -0400

    Added comments to CasualGarmentFactory class

--- src/main/java/com/company/garmentsimulator/CasualGarmentFactory.java
+++ src/main/java/com/company/garmentsimulator/CasualGarmentFactory.java
@@ -1,16 +1,33 @@
 package com.company.garmentsimulator;
 
+/**
+ * The CasualGarmentFactory class creates casual tops, pants, and shoes
+ * @author Chris Masoud
+ */
 public class CasualGarmentFactory implements GarmentFactory {
+    
+    /**
+     * The createTop() method returns a new casual top
+     * @return Top
+     */
     @Override
     public Top createTop() {
         return new CasualTop();
     }
 
+    /**
+     * The createPants() method returns new casual pants
+     * @return Pants
+     */
     @Override
     public Pants createPants() {
         return new CasualPants();
     }
-
+    
+    /**
+     * The createShoes() method returns new casual shoes
+     * @return Shoes
+     */
     @Override
     public Shoes createShoes() {
         return new CasualShoes();
