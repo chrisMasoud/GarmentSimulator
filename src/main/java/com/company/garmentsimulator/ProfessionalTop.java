1693742961d1df17d1d518e7e65bef620085ccec
chrisMasoud <chrismasoud22@gmail.com>
2023-03-30 10:33:34 -0400

    Created ProfessionalTop class

---
+++ src/main/java/com/company/garmentsimulator/ProfessionalTop.java
@@ -0,0 +1,8 @@
+package com.company.garmentsimulator;
+
+public class ProfessionalTop implements Top {
+    @Override
+    public void sew() {
+        System.out.println("Sewing a professional top...");
+    }
+}
