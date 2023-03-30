e96eab541545812491474e28ee51f2328a3cdbae
chrisMasoud <chrismasoud22@gmail.com>
2023-03-30 11:01:10 -0400

    Added comments to PartyShoes class

--- src/main/java/com/company/garmentsimulator/PartyShoes.java
+++ src/main/java/com/company/garmentsimulator/PartyShoes.java
@@ -1,6 +1,14 @@
 package com.company.garmentsimulator;
 
+/**
+ * The PartyShoes class sews party shoes
+ * @author Chris Masoud
+ */
 public class PartyShoes implements Shoes {
+    
+    /**
+     * The sew() method outputs that it is sewing party shoes
+     */
     @Override
     public void sew() {
         System.out.println("Sewing party shoes...");
